0737f8d0c5739068d6d3ca0e6aeba8056076a297
instory2017 <instory2017@daum.net>
2026-02-01 17:24:15 +0900

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,1 +1,54 @@
+const CACHE_NAME = "maetan-parish-v1";
 
+const ASSETS = [
+  "./",
+  "./index.html",
+  "./about.html",
+  "./gallery.html",
+  "./bulletin.html",
+  "./notice.js",
+  "./manifest.webmanifest",
+  "./icon-180.png",
+  "./icon-192.png",
+  "./icon-512.png",
+  "./maetan_v2.jpg",
+  "./maetan-mobile_v2.jpg",
+  "./assets/link-about.jpg",
+  "./assets/link-bulletin.jpg",
+  "./assets/link-gallery.jpg",
+  "./assets/link-notice.jpg"
+];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(keys.filter((k) => k !== CACHE_NAME).map((k) => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", (event) => {
+  const req = event.request;
+  if (req.method !== "GET") return;
+
+  event.respondWith(
+    caches.match(req).then((cached) => {
+      if (cached) return cached;
+      return fetch(req)
+        .then((res) => {
+          const copy = res.clone();
+          caches.open(CACHE_NAME).then((cache) => cache.put(req, copy));
+          return res;
+        })
+        .catch(() => caches.match("./index.html"));
+    })
+  );
+});
